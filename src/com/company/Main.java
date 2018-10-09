e79d2bacb88ac7f8716c49255a4388699811e5b4
GBTC406005UR <GBTC406005UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-09 16:44:43 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // 1. Create an array to hold the numbers 1-10.
+        //2. Loop through that array and sum the numbers 1-10, then print that total to the screen.
+        int []num1={1,2,3,4,5,6,7,8,9,10};
+        for(int i=1;i<num1.length +1;i++){
+            System.out.println(i);
+        }
+    }
+}
